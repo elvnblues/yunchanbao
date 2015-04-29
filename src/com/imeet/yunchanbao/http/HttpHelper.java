a0adbf68a9661790d8d715587edfd72ef9bfa31d
elvnblues <elvnblues@gmail.com>
2015-04-29 15:28:19 +0800

    Update http code

--- src/com/imeet/yunchanbao/http/HttpHelper.java
+++ src/com/imeet/yunchanbao/http/HttpHelper.java
@@ -4,6 +4,8 @@ package com.imeet.yunchanbao.http;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -15,7 +17,6 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
@@ -41,7 +42,7 @@ public class HttpHelper {
 	public static final int TIMEOUT_DURATION=5000;
 	//超时提示
 	public static final String TIMEOUT = "timeout";
-	/**json解析错误*/
+	/**jso n解析错误*/
 	public static final String JSON_ERROR = "jsonerror";
 	
 	/**标记为请求完*/
@@ -144,7 +145,47 @@ public class HttpHelper {
 		
 		return result;
 	}	
-	
+	/**
+	 * 从服务器请求获取到JSON数据格式的字符串
+	 * @param url_path
+	 * @return
+	 */
+	public static String getJsonContent(String url_path) {
+        try {
+            URL url = new URL(url_path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(3000); // 请求超时时间3s
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            int code = connection.getResponseCode(); // 返回状态码
+            if (code == 200) {
+                // 或得到输入流，此时流里面已经包含了服务端返回回来的JSON数据了,此时需要将这个流转换成字符串
+                return changeInputStream(connection.getInputStream());
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return "";
+    }
+
+    private static String changeInputStream(InputStream inputStream) {
+        // TODO Auto-generated method stub
+        String jsonString = "";
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        int length = 0;
+        byte[] data = new byte[1024];
+        try {
+            while (-1 != (length = inputStream.read(data))) {
+                outputStream.write(data, 0, length);
+            }
+            // inputStream流里面拿到数据写到ByteArrayOutputStream里面,
+            // 然后通过outputStream.toByteArray转换字节数组，再通过new String()构建一个新的字符串。
+            jsonString = new String(outputStream.toByteArray());
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return jsonString;
+    }
 	/**
 	 * 判断网络是否可用
 	 * @param act
